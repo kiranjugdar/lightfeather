efa64df56c23ffa1085b5b4aa4e460d951f4b42e
kiran <kiran@kirans-mbp.lan>
2024-02-18 10:17:07 -0500

    Swagger documentation added on API

--- src/main/java/io/lightfeather/management/controller/ManagementController.java
+++ src/main/java/io/lightfeather/management/controller/ManagementController.java
@@ -2,6 +2,10 @@ package io.lightfeather.management.controller;
 
 import io.lightfeather.management.model.UserDetails;
 import io.lightfeather.management.service.ManagementService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "Management", description = "API for management service")
 public class ManagementController {
 
     @Autowired
@@ -21,11 +26,24 @@ public class ManagementController {
 
     Logger logger = LoggerFactory.getLogger(ManagementController.class);
 
+
+    @Operation(
+            summary = "Get list of supervisors",
+            description = "Returns list of supervisors Jurisdiction - LastName, FirstName")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation")
+    })
     @GetMapping("/supervisors")
     public List<String> getSupervisors() {
         return managementService.getSupervisors();
     }
 
+    @Operation(
+            summary = "Submit supervisor",
+            description = "Validates required information passed to supervisor and submit")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation")
+    })
     @PostMapping("/submit")
     public void submit(@Valid @RequestBody UserDetails userDetails) {
         logger.info("Submitted data: {}", userDetails);
